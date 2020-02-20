6f61f0111b09399acadcb2ecaa01900d760bcc21
EmNudge <emnudge@gmail.com>
2020-02-20 13:38:37 -0500

    moved remap to general utils

--- src/audio/remap.js
+++
@@ -1,9 +0,0 @@
-// usually called "map", but remap makes more sense. 
-// It maps one range onto another.
-function remap(val, min, max, min2, max2) {
-  const basePercentage = (val - min) / (max - min);
-
-  return basePercentage * (max2 - min2) + min2;
-}
-
-export default remap

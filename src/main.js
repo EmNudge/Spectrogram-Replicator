91dd9710e94e47d3267591f350567845f11ae465
EmNudge <emnudge@gmail.com>
2020-05-29 22:45:31 -0400

    converted to PWA

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import App from './App.svelte';
 
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/service-worker.js')
+		.then(() => console.log('Service Worker Registered'));
+}
+
 const app = new App({
 	target: document.body,
 	props: {

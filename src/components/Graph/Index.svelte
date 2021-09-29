eb00c41e240f39f95e50432b1df8cae7655b4824
EmNudge <emnudge@gmail.com>
2021-09-29 17:48:44 -0400

    add lines

--- src/components/Graph/Index.svelte
+++ src/components/Graph/Index.svelte
@@ -1,23 +1,75 @@
-<script>
-    let rows;
-    let columns;
+<script lang="ts">
+    let rows = 8;
+    let columns = 3;
+
+    let maxFreq = 3000;
+    const formatHz = (row: number) => {
+        const freq = maxFreq / rows * row;
+        const maxSize = Math.floor(Math.log10(maxFreq) + 1);
+        return `${freq.toFixed(1).padStart(maxSize, '0')} Hz`;
+    }
+    
 </script>
 
-<svg class="frequencies">
-
-</svg>
+<div class="frequencies">
+    {#each Array(rows) as _, i}
+        <span style="">{formatHz(rows - i)}</span>
+    {/each}
+</div>
 
 <svg class="graph">
-
+    <g class="rows">
+        {#each Array(rows) as _, i}
+            {#if i > 0}
+                <line
+                    x1="0%"
+                    x2="100%"
+                    y1="{i/rows * 100}%" 
+                    y2="{i/rows * 100}%"/>
+            {/if}
+        {/each}
+    </g>
+    <g>
+        {#each Array(columns) as _, i}
+            {#if i > 0}
+                <line
+                    y1="0%"
+                    y2="100%"
+                    x1="{i/columns * 100}%" 
+                    x2="{i/columns * 100}%"/>
+            {/if}
+        {/each}
+    </g>
 </svg>
 
 <style>
     .frequencies, .graph {
         height: 500px;
     }
+    .frequencies {
+        display: grid;
+    }
+    .frequencies span {
+        margin-top: -10px;
+        font-size: 14px;
+        text-align: right;
+        font-weight: bold;
+        color: #000a;
+    }
+    .frequencies span:nth-child(even) {
+        color: #0005;
+    }
     svg {
         background: white;
         width: 100%;
         border-radius: 4px;
+    }
+    svg line {
+        stroke-width: 2;
+        stroke-dasharray: 15;
+        stroke: #999;
+    }
+    .rows line:nth-child(odd) {
+        stroke: #9994;
     }
 </style>

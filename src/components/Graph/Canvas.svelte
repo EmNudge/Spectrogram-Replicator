bdc693ecfea61c737e376e412c486b4f92339a65
EmNudge <emnudge@gmail.com>
2021-09-30 18:45:34 -0400

    add lines to canvas

--- src/components/Graph/Canvas.svelte
+++ src/components/Graph/Canvas.svelte
@@ -1,15 +1,27 @@
 <script>
     import { pointsSt } from '../../stores/canvas';
+
+    $: points = $pointsSt;
 </script>
 
-{#each $pointsSt as { x, y }}
+{#each Array(points.length - 1) as _, i}
+    <line
+        x1="{points[i].x * 100}%"
+        y1="{points[i].y * 100}%"
+        
+        x2="{points[i+1].x * 100}%"
+        y2="{points[i+1].y * 100}%"    
+    ></line>
+{/each}
+
+{#each points as { x, y }, i}
     <circle
         cx="{x * 100}%"
         cy="{y * 100}%"></circle>
 {/each}
 
 <style>
-    circle {
+    circle, line {
         r: 5;
         stroke-width: 3px;
         stroke: blue;
--- src/components/SideMenu/Index.svelte
+++ src/components/SideMenu/Index.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { SvelteComponent } from 'svelte';
+	import type { SvelteComponent } from 'svelte';
 	import { tabs, activeTabSt } from '../../stores/menus';
 
 	import Main from './Main.svelte';

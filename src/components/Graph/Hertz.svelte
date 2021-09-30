6ead75abba00d0d0f3f2748f43cf0f9ed556a6d3
EmNudge <emnudge@gmail.com>
2021-09-30 18:13:37 -0400

    split up lines and hertz

---
+++ src/components/Graph/Hertz.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { rowsSt, maxFreqSt, lightenOddRowsSt, minFreqSt } from '../../stores/graph';
+
+    $: delta = $maxFreqSt - $minFreqSt;
+    $: maxSize = Math.floor(Math.log10($maxFreqSt) + 1);
+    $: HzArr = Array.from(
+        { length: $rowsSt }, 
+        (_, i) => {
+            const rowPerc = ($rowsSt - i) / $rowsSt;
+            const freq = $minFreqSt + delta * rowPerc;
+            return `${freq.toFixed(1).padStart(maxSize, '0')} Hz`;
+        });
+</script>
+
+<div class="frequencies" class:lighten-odd={$lightenOddRowsSt}>
+    {#each HzArr as hz}
+        <span>{hz}</span>
+    {/each}
+</div>
+
+<style>
+    .frequencies {
+        height: 500px;
+    }
+    .frequencies {
+        display: grid;
+    }
+    .frequencies span {
+        margin-top: -10px;
+        font-size: 14px;
+        text-align: right;
+        font-weight: bold;
+        color: #000a;
+    }
+
+    .lighten-odd.frequencies span:nth-child(even) {
+        color: #0005;
+    }
+</style>
---
+++ src/components/Graph/Lines.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { rowsSt, columnsSt, showGridst, lightenOddRowsSt } from '../../stores/graph';
+</script>
+
+{#if $showGridst}
+    <g class="rows" class:lighten-odd={$lightenOddRowsSt}>
+        {#each Array($rowsSt) as _, i}
+            {#if i > 0}
+                <line
+                    x1="0%"
+                    x2="100%"
+                    y1="{i/$rowsSt * 100}%" 
+                    y2="{i/$rowsSt * 100}%"/>
+            {/if}
+        {/each}
+    </g>
+    <g>
+        {#each Array($columnsSt) as _, i}
+            {#if i > 0}
+                <line
+                    y1="0%"
+                    y2="100%"
+                    x1="{i/$columnsSt * 100}%" 
+                    x2="{i/$columnsSt * 100}%"/>
+            {/if}
+        {/each}
+    </g>
+{/if}
+
+<style>
+    .lighten-odd line:nth-child(odd) {
+        stroke: #9994;
+    }
+</style>

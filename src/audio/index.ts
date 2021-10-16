618a74149979bb7492eaefe0c1d99c661f00dae6
EmNudge <emnudge@gmail.com>
2021-10-16 14:57:13 -0400

    move to schedule system

--- src/audio/index.ts
+++ src/audio/index.ts
@@ -6,6 +6,64 @@ import { get } from "svelte/store";
 const GLOBAL_VOLUME = .2;
 const DECAY = .01;
 
+enum ScheduleType {
+    GAIN_NODE,
+    FREQ_NODE,
+}
+const getGainSchedule = (value: number, time: number) => 
+    ({ value, time, type: ScheduleType.GAIN_NODE });
+const getFreqSchedule = (value: number, time: number) => 
+    ({ value, time, type: ScheduleType.FREQ_NODE });
+
+interface ScheduleNode {
+    type: ScheduleType,
+    value: number,
+    time: number,
+}
+export function* getSchedule(line: Line, startPerc = 0): Generator<ScheduleNode> {
+    const segments = get(line.segmentsSt);
+    const minFreq = get(minFreqSt);
+    const maxFreq = get(maxFreqSt);
+    const freqDelta = maxFreq - minFreq;
+    const duration = get(durationSt);
+
+    let hasStarted = false;
+
+    for (const segment of segments) {
+        const points = get(segment.pointsSt);
+
+        // ramp up volume
+        if (hasStarted) {
+            const firstTime = points[0].x * duration;
+            yield getGainSchedule(0, firstTime - DECAY);
+            yield getGainSchedule(1, firstTime);
+        }
+
+        for (const point of points) {
+            if (point.x < startPerc) continue;
+
+            const frequency = point.y * freqDelta + minFreq;
+            const time = point.x * duration;
+
+            // ramp up volume
+            if (!hasStarted) {
+                hasStarted = true;
+                yield getGainSchedule(0, time - DECAY);
+                yield getGainSchedule(1, time);
+            }
+
+            yield getFreqSchedule(frequency, time);
+        }
+
+        // ramp down volume
+        if (hasStarted) {
+            const lastTime = points[points.length - 1].x * duration;
+            yield getGainSchedule(1, lastTime);
+            yield getGainSchedule(0, lastTime + DECAY);
+        }
+    }
+}
+
 export function playLine(line: Line) {
     const audioContext = new AudioContext();
 
@@ -23,27 +81,12 @@ export function playLine(line: Line) {
     oscillatorNode.connect(segmentGainNode);
     oscillatorNode.start(0);
 
-    const minFreq = get(minFreqSt);
-    const maxFreq = get(maxFreqSt);
-    const freqDelta = maxFreq - minFreq;
-    const duration = get(durationSt);
-
-    const segments = get(line.segmentsSt);
-    for (const segment of segments) {
-        const points = get(segment.pointsSt);
-
-        const firstTime = now + points[0].x * duration;
-        segmentGainNode.gain.linearRampToValueAtTime(0, firstTime - DECAY);
-        segmentGainNode.gain.linearRampToValueAtTime(1, firstTime);
-
-        for (const point of points) {
-            const frequency = point.y * freqDelta + minFreq;
-            const time = point.x * duration;
-            oscillatorNode.frequency.linearRampToValueAtTime(frequency, now + time);
+    for (const { type, time, value } of getSchedule(line)) {
+        if (type === ScheduleType.GAIN_NODE) {
+            segmentGainNode.gain.linearRampToValueAtTime(value, now + time);
+        } else {
+            oscillatorNode.frequency.linearRampToValueAtTime(value, now + time);
         }
-        const lastTime = now + points[points.length - 1].x * duration;
-        segmentGainNode.gain.linearRampToValueAtTime(1, lastTime);
-        segmentGainNode.gain.linearRampToValueAtTime(0, lastTime + DECAY);
     }
 
     return audioContext;
